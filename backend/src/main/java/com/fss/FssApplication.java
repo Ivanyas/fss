ef5e128813b6b3f0dd119dacda8cfde728c02778
Ivan Ivanyas <ivanyasivan1@gmail.com>
2026-02-12 13:09:53 +0300

    init spring boot project

---
+++ backend/src/main/java/com/fss/FssApplication.java
@@ -0,0 +1,12 @@
+package com.fss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FssApplication.class, args);
+    }
+}
